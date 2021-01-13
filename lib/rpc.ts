58edd38bcd2ca08491892f61925a855edbcfd40f
surinder singh <surinder83singh@gmail.com>
2021-01-13 07:29:35 +0530

    client side unSubscribe() support

--- lib/rpc.ts
+++ lib/rpc.ts
@@ -14,7 +14,9 @@ export class RPC implements IRPC{
 	disconnect(){
 		this.client?.disconnect();
 	}
-
+	unSubscribe(method:string, uid:string=''){
+		return this.client.unSubscribe(method, uid);
+	}
 	subscribe<T, R>(method:string, data:any, callback:Rpc.callback<R>){
 		return this.client.subscribe<T>(method, data, callback);
 	}
@@ -34,6 +36,10 @@ export class RPC implements IRPC{
 
 	subscribeUtxosChanged(addresses:string[], callback:Rpc.callback<Rpc.UtxosChangedNotification>){
 		return this.subscribe<Rpc.NotifyUtxosChangedResponse, Rpc.UtxosChangedNotification>("notifyUtxosChangedRequest", {addresses}, callback);
+	}
+
+	unSubscribeUtxosChanged(uid:string=''){
+		this.unSubscribe("notifyUtxosChangedRequest", uid);
 	}
 
 	getBlock(hash:string){
